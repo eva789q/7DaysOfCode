6ccb225ca06f87fd90eddbc8cc933d5da87a8932
eva789q <contacto@almargen.cl>
2025-03-06 01:22:13 -0300

    Corrigiendo la conexión con GitHub

--- day2.js
+++ day2.js
@@ -1,34 +1,13 @@
 let nombre = prompt("Escribe tu ciudad:");
 let edad =  prompt("Escribe tu edad:");
-let lengueje =  prompt("Escribe tu ciudad:");
+let lenguaje =  prompt("¿Qué lenguaje de programación estás estudiando?");
+let mensaje = `Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${lenguaje}!`;
 
 
 
-- ¿Cuál es tu nombre?
-- ¿Cuántos años tienes?
-- ¿Qué lenguaje de programación estás estudiando?
-
- 
-
-A medida que se hagan las preguntas, la persona que esté usando el programa debe responder cada una de ellas.
-
- 
-
-Al final, el sistema mostrará el mensaje:
-
- 
-
-"Hola [nombre], tienes [edad] años y ya estás aprendiendo [lenguaje]!"
-
- 
-
-Observa que cada información entre [ ] es una de las respuestas dadas por la persona.
 
 
 
-const ciudad = prompt("Escribe tu ciudad:");
 
-const msg = `¡Eres de ${ciudad}!`;
 
-alert(msg);
-
+alert(mensaje);
